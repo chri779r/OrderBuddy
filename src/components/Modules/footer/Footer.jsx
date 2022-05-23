4e622fc552caa65e6884e6b8e756017e343296db
Katrín María <katr240j@stud.kea.dk>
2022-05-23 18:48:34 +0200

    footer prove, pusher til at kunne arbejde videre

--- src/components/Modules/footer/Footer.jsx
+++ src/components/Modules/footer/Footer.jsx
@@ -1,1 +1,15 @@
 import "./Footer.scss"
+
+
+export default function Footer() {
+    return (
+   <section className="footer">
+       <div className="vigtige_links">
+           <h4>Vigtige links</h4>
+           <a href=""></a>
+       </div>
+       <div className="kontakt"></div>
+       <div className="some_links"></div>
+   </section>
+    )
+  }
