a167c09ac1f76f26102be135b1fb487c768e2625
Katrín María <katr240j@stud.kea.dk>
2022-05-23 21:36:23 +0200

    footer indhold 2

--- src/components/Modules/footer/Footer.jsx
+++ src/components/Modules/footer/Footer.jsx
@@ -1,15 +1,39 @@
 import "./Footer.scss"
-
+import { Logo } from "../logo/logo"
+import { Facebook } from "../icons/icons"
+import { Instagram } from "../icons/icons"
+import { Linkedin } from "../icons/icons"
 
 export default function Footer() {
     return (
    <section className="footer">
+       <div className="footer_container">
        <div className="vigtige_links">
            <h4>Vigtige links</h4>
-           <a href=""></a>
+    <ul>
+         <li className="footer_link_item"><a href="#">Om OrderBuddy</a></li>
+         <li className="footer_link_item"><a href="#">Find os her</a></li>
+         <li className="footer_link_item"><a href="#">FAQ</a></li>
+         <li className="footer_link_item"><a href="#">Kontakt OrderBuddy</a></li>
+     </ul>
        </div>
-       <div className="kontakt"></div>
-       <div className="some_links"></div>
+       <div className="kontakt">
+           <h4>Kontakt</h4>
+           <p>OrderBuddy ApS
+               Vinkelager 3, st. th.
+           2720 Vanløse</p>
+           <p>info@orderbuddy.dk</p>
+           <p>+45 22121242</p>
+       </div>
+       </div>
+       <div className="some_links">
+       <Logo />
+       <div className="icon_footer">
+       <Facebook />
+        <Instagram />
+       <Linkedin /> 
+       </div>
+       </div>
    </section>
     )
   }
