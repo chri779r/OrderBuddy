4d9e4e1404dc964c87a4728f0e054843beabb88c
Christian Munch <chri779r@stud.kea.dk>
2022-05-23 17:13:30 +0200

    working on mobile nav styling

--- src/components/Modules/nav/navlinks.jsx
+++ src/components/Modules/nav/navlinks.jsx
@@ -4,7 +4,7 @@ import "./navLinks.scss"
 export function NavLinks(props) {
   return (
     <div className="links__container">
-        <ul className="link__list">
+        <ul className="links__list">
             <li className="link__item"><a href="#">Hjem</a></li>
             <li className="link__item"><a href="#">Hvordan</a></li>
             <li className="link__item"><a href="#">Find os</a></li>
