4e9f60ed0b3454d0d293124d8b7992b5d081735b
Christian Munch <chri779r@stud.kea.dk>
2022-05-25 21:33:24 +0200

    forgot new branch again....

--- src/components/Modules/pages/omos.jsx
+++ src/components/Modules/pages/omos.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { WhoHero } from '../heros2/heroAbout'
-import { Button } from '../button/button'
 
 export default function OmosPage(){
     return(
       <>
       <WhoHero/>
       <div className="card__holder">
-        <Button><span>test knap</span></Button>
+        
       </div>
       </>
     )
