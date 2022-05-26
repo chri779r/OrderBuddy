d9916e4559090059029edaa9430115914a62feb4
Christian Munch <chri779r@stud.kea.dk>
2022-05-26 09:18:09 +0200

    hjemcard

--- src/components/Modules/cards/cards.jsx
+++ src/components/Modules/cards/cards.jsx
@@ -1,3 +1,23 @@
 import React from "react";
 import "./cards.scss"
 
+export default function Card() {
+    function CardHeader(){
+        return(
+            <h3></h3>
+        )
+    }
+    function CardText(){
+        return(
+            <p></p>
+        )
+    }
+
+
+    return(
+        <>
+            <CardHeader/>
+            <CardText/>
+        </>
+    )
+}
