bf72c4d9bfeb0232968742661db34a93249fce85
Christian Munch <chri779r@stud.kea.dk>
2022-05-26 11:17:22 +0200

    base card works

--- src/components/Modules/button/button.jsx
+++ src/components/Modules/button/button.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import styled from "styled-components";
+import "./button.scss"
+// import styled from "styled-components";
 
-const Btn = styled.button`
-    height: 3em;
-    width: auto;
-    padding: 1em 2em;
-    margin: 1em 0;
-    font-family: "roboto", san-serif;
-    font-size: 1em;
-    background-color: var(--lilac);
-    color: var(--white);
-    transform: scaleX(0);
-    transform-origin: right;
-    transition: transform .5s ease;
-    &:hover {
-        background-color: var(--indigo);
-        transform-origin: left;
-        transform: scaleX(1);
-    }
-`;
+// const Btn = styled.button`
+//     height: 3em;
+//     width: auto;
+//     padding: 1em 2em;
+//     margin: 1em 0;
+//     font-family: "roboto", san-serif;
+//     font-size: 1em;
+//     background-color: var(--lilac);
+//     color: var(--white);
+//     transform: scaleX(0);
+//     transform-origin: right;
+//     transition: transform .5s ease;
+
+//     &:hover {
+//         background-color: var(--indigo);
+//         transform-origin: left;
+//         transform: scaleX(1);
+//     }
+// `;
 
 
-export function Button() {
+export default function Button({text}) {
 
     return(
-        <Btn/>
+        <button className="btn">{text}</button>
     )
 }
--- src/components/Modules/cards/cards.jsx
+++ src/components/Modules/cards/cards.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import "./cards.scss"
+// import React from "react";
+// import "./cards.scss"
 
-export default function Card() {
-    function CardHeader(){
-        return(
-            <h3></h3>
-        )
-    }
-    function CardText(){
-        return(
-            <p></p>
-        )
-    }
+// export default function Card() {
+//     function CardHeader(){
+//         return(
+//             <h3></h3>
+//         )
+//     }
+//     function CardText(){
+//         return(
+//             <p></p>
+//         )
+//     }
 
 
-    return(
-        <>
-            <CardHeader/>
-            <CardText/>
-        </>
-    )
-}
+//     return(
+//         <>
+//             <CardHeader/>
+//             <CardText/>
+//         </>
+//     )
+// }
