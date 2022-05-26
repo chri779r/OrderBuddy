7125cdc08b6071d28c3ae96ef9319e3037c45283
Katrín María <katr240j@stud.kea.dk>
2022-05-27 01:18:14 +0200

    form

--- src/components/Modules/pages/kontakt.jsx
+++ src/components/Modules/pages/kontakt.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { ContactHero } from "../heros2/heroContact"
+import ContactForm from "../form/form"
 
 function KontaktPage(){
     return(
       <>
         <ContactHero/>
+        <ContactForm/>
       </>
     
     )
