f14f91d1b619a53dd380a682732016d317f78394
Katrín María <katr240j@stud.kea.dk>
2022-05-26 22:28:59 +0200

    ny map

--- src/components/Modules/googleMaps/googleMaps.jsx
+++ src/components/Modules/googleMaps/googleMaps.jsx
@@ -8,7 +8,7 @@ import './googleMaps.scss'
           <div className="container">
         <h3 className="header">Søg på stedet</h3>
         <p className="text">Kan du ikke finde stedet du søger på kortet? Prøv at skrive navnet på den café eller restaurant du søger og se efter resultatet under.</p>
-        <iframe className="map" src="https://storage.googleapis.com/maps-solutions-a4wpvqs0s9/locator-plus/2tmx/locator-plus.html"   loading="lazy"></iframe>
+        <iframe className="map" src="https://storage.googleapis.com/maps-solutions-a4wpvqs0s9/locator-plus/o6r5/locator-plus.html"   loading="lazy"></iframe>
       </div>
       </div>
     );
