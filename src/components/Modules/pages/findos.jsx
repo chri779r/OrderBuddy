689ee1a4092cc1e12554e0e4b91a00b08ee7afd2
Katrín María <katr240j@stud.kea.dk>
2022-05-26 21:48:49 +0200

    google maps losning

--- src/components/Modules/pages/findos.jsx
+++ src/components/Modules/pages/findos.jsx
@@ -1,9 +1,12 @@
 import { FindHero } from "../heros2/heroFind"
+import Map from "../googleMaps/googleMaps"
 
 function FindPage(){
     return(
         <>
             <FindHero/>
+            <Map/>
+
         </>
     )
 }
