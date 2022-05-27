79fdb33eabb5f1432bcf024662cffdbc39faa5ec
Christian Munch <chri779r@stud.kea.dk>
2022-05-27 18:20:44 +0200

    nav active styling works

--- src/components/Modules/nav/navlinks.jsx
+++ src/components/Modules/nav/navlinks.jsx
@@ -10,9 +10,9 @@ export function NavLinks(props) {
         <ul className="links__list">
             <li className="link__item"><Link to="/" className={location.pathname==="/"?"active":""}>Hjem</Link></li>
             <li className="link__item"><Link to="/hvordan" className={location.pathname==="/hvordan"?"active":""}>Hvordan</Link></li>
-            <li className="link__item"><Link to="/find">Find os</Link></li>
-            <li className="link__item"><Link to="/omos">Om os</Link></li>
-            <li className="link__item"><Link to="/kontakt">Kontakt</Link></li>
+            <li className="link__item"><Link to="/find" className={location.pathname==="/find"?"active":""}>Find os</Link></li>
+            <li className="link__item"><Link to="/omos" className={location.pathname==="/omos"?"active":""}>Om os</Link></li>
+            <li className="link__item"><Link to="/kontakt" className={location.pathname==="/kontakt"?"active":""}>Kontakt</Link></li>
         </ul>
     </div>
   )
