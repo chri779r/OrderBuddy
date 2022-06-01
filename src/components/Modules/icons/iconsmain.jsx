e1c9b1b8daa83e26e8d9f1a2b78c1aab0f2f3fbf
Christian Munch <chri779r@stud.kea.dk>
2022-06-01 11:59:40 +0200

    paths

--- src/components/Modules/icons/iconsmain.jsx
+++ src/components/Modules/icons/iconsmain.jsx
@@ -12,51 +12,57 @@ import './iconsmain.scss'
 
 const IconMain = () => {
     return(
-   
-        <div className="IconsWrapper" >
-        <div className="IconsContainer" >
-        <div className="Wrapper" >
-        <div className="IconContainer" >
-        <figure className="ikon__img">
-            <img src={ikonKlok} alt="ikon klok OrderBuddy" />
-          </figure>
-        </div>
-            <h3>Ingen ventetid</h3> 
-            <p>Alle kender det, at tjeneren ikke har tid. Blikkene flakker forgæves for at få kontakt. Løsningen er OrderBuddy </p>
-        </div>
+    <>
+        <section className="icon__wrapper">
+            <div className="icon__container">
+                <div className="icon__img">
+                    <figure>
+                        <img src={ikonKlok} alt="ikon klok OrderBuddy" />
+                    </figure>
+                </div>
+                <div className="icon__info">
+                    <h3>Ingen ventetid</h3> 
+                    <p>Alle kender det, at tjeneren ikke har tid. Blikkene flakker forgæves for at få kontakt. Løsningen er OrderBuddy </p>
+                </div>
+            </div>
+            
+            <div className="icon__container">
+                <div className="icon__img">
+                    <figure>
+                        <img src={ikonKort} alt="ikon kort OrderBuddy" />
+                    </figure>
+                </div>
+                <div className="icon__info">
+                    <h3>Mindre CO2</h3>
+                    <p>Med OrderBuddy får du kvitteringen digitalt og slipper for at skabe unøddigt prints, der alligevel ender i skraldespanden.</p>
+                </div>
+            </div>
 
-        <div className="Wrapper" >
-        <div className="IconContainer" >
-        <figure className="ikon__img">
-            <img src={ikonKort} alt="ikon kort OrderBuddy" />
-          </figure>
-        </div>
-            <h3>Bestil lige når du ønsker</h3> 
-            <p>Læn dig tilbage, nyd selskabet og bestil og betal når du er klar, køkkenet eller baren modtager din bestilling.</p>
-        </div>
-
-        <div className="Wrapper" >
-        <div className="IconContainer" >
-        <figure className="ikon__img">
-            <img src={ikonRabat} alt="ikon rabat OrderBuddy" />
-          </figure>
-        </div>
-            <h3>Kundeklubben</h3>           
-            <p>Modtag automatisk rabat og stempelkort fra dine yndlingssteder direkte i app’en</p>
-        </div>
-
-        <div className="Wrapper" >
-        <div className="IconContainer" >
-        <figure className="ikon__img">
-            <img src={ikonCo} alt="ikon CO2 OrderBuddy" />
-          </figure>
-        </div>
-        <h3>Mindre CO2</h3>           
-            <p>Med OrderBuddy får du kvitteringen digitalt og slipper for at skabe unøddigt prints, der alligevel ender i skraldespanden.</p>
-        </div>
-        </div>
-        </div>
- 
+            <div className="icon__container">
+                <div className="icon__img">
+                    <figure>
+                        <img src={ikonRabat} alt="ikon rabat OrderBuddy" />
+                    </figure>
+                </div>
+                <div className="icon__info">
+                    <h3>Kundeklubben</h3>           
+                    <p>Modtag automatisk rabat og stempelkort fra dine yndlingssteder direkte i app’en</p>
+                </div>
+            </div>
+            
+            <div className="icon__container">
+                <div className="icon__img">
+                    <figure>
+                        <img src={ikonCo} alt="ikon CO2 OrderBuddy" />
+                    </figure>
+                </div>
+                <div className="icon__info">
+                    <h3>Mindre CO2</h3>
+                    <p>Med OrderBuddy får du kvitteringen digitalt og slipper for at skabe unøddigt prints, der alligevel ender i skraldespanden.</p>
+                </div>
+            </div>
+        </section>
+    </> 
     )
 }
 
